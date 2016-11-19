e7817828ff4ff24e1eeee7522520aa078bcece6e
Suhwan Cha <admin@gdb.kr>
2016-11-19 11:02:18 +0900

    profile page

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="../assets/img/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>About | Suhwan Cha</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+	<!-- CSS Files -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/material-kit.css" rel="stylesheet"/>
+		<link href='../assets/css/gdb.kr.css' rel='stylesheet' />
+
+</head>
+
+<body class="profile-page">
+	<nav class="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll navbar-info">
+    	<div class="container">
+        	<!-- Brand and toggle get grouped for better mobile display -->
+        	<div class="navbar-header">
+        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+            		<span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+        		</button>
+        		<a class="navbar-brand" href="/">gdb.kr Co.</a>
+        	</div>
+
+        	<div class="collapse navbar-collapse" id="navigation-example">
+        		<ul class="nav navbar-nav navbar-right">
+				   				<li>
+          					<a href="#" target="_blank">
+          						<i class="material-icons">code</i>Portfolio
+          					</a>
+          				</li>
+          				<li>
+          					<a href="#" target="_blank">
+          						<i class="material-icons">chat</i> Connect
+          					</a>
+          				</li>
+          				<li>
+          					<a rel="tooltip" title="Connect with Facebook" data-placement="bottom" href="https://www.facebook.com/profile.php?id=100003860414593" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+          						<i class="fa fa-facebook-square"></i>
+          					</a>
+          				</li>
+        		</ul>
+        	</div>
+    	</div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,201 @@
+<?php require_once('./header.php'); ?>
+
+    <div class="wrapper">
+		<div class="header header-filter" style="background-image: url('https://home.gdb.kr/img/bg.jpg');">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="brand">
+              <h1>gdb.kr Co</h1>
+              <h3>admin - Suhwan Cha</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+		<div class="main main-raised">
+			<div class="profile-content">
+        <div class="container">
+            <div class="row">
+              <div class="profile">
+                <div class="avatar">
+                  <img src="../assets/img/icon.png" alt="Circle Image" class="img-circle img-responsive img-raised">
+                </div>
+
+                <div class="name">
+                  <h3 class="title">Suhwan Cha</h3>
+        					<h6>Developer</h6>
+                </div>
+              </div>
+            </div>
+            <div class="description text-center">
+                  <p>Full-stack web Developer</p>
+            </div>
+
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3">
+							<div class="profile-tabs">
+			                    <div class="nav-align-center">
+									<ul class="nav nav-pills nav-pills-info" role="tablist">
+										<li class="active">
+											<a href="#front" role="tab" data-toggle="tab">
+                        <i class="material-icons">palette</i>
+												Front-end
+											</a>
+										</li>
+										<li>
+			                <a href="#back" role="tab" data-toggle="tab">
+												<i class="material-icons">code</i>
+												Back-end
+			                </a>
+				            </li>
+				            <li>
+				              <a href="#hacking" role="tab" data-toggle="tab">
+                      <i class="material-icons">fingerprint</i>
+				                                        Hacking
+				              </a>
+				            </li>
+				          </ul>
+
+				          <div class="tab-content gallery">
+										<div class="tab-pane active" id="front">
+				              <div class="row">
+												<div class="col-md-6">
+													<img src="../assets/img/html5.png" class="img-rounded" />
+                          <img src="../assets/img/jquery.png" class="img-rounded" />
+												</div>
+                        <div class='col-md-6'>
+                          <img src="../assets/img/css3.png" class="img-rounded"/>
+                          <img src="../assets/img/photoshop.jpg" class="img-rounded"/>
+
+                        </div>
+                      </div>
+				            </div>
+				            <div class="tab-pane text-center" id="back">
+											<div class="row">
+												<div class="col-md-6">
+													<img src="../assets/img/php.png" class="img-rounded" />
+													<img src="../assets/img/laravel.png" class="img-rounded" />
+												</div>
+												<div class="col-md-6">
+													<img src="../assets/img/mysql.png" class="img-rounded" />
+													<img src="../assets/img/ubuntu.png" class="img-rounded" />
+												</div>
+											</div>
+				                        </div>
+										<div class="tab-pane text-center" id="hacking">
+											<div class="row">
+												<div class="col-md-6">
+                          <h3>System Hacking</h3>
+                          HackerSchool FTZ
+                          <div class="progress">
+                          	<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                          	<span class="sr-only">100% Complete</span>
+                          	</div>
+                          </div>
+                          Lord of BoF
+                          <div class="progress">
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                            <span class="sr-only">100% Complete</span>
+                            </div>
+                          </div>
+                          pwnable.kr
+                          <div class="progress">
+                            <div class="progress-bar progress-bar-warn" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 10%;">
+                            <span class="sr-only">10% Complete</span>
+                            </div>
+                          </div>
+
+                          <h3>Reversing</h3>
+                          Reversing.kr
+                          <div class="progress">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+                            <span class="sr-only">70% Complete</span>
+                            </div>
+                          </div>
+                          CodeEngin
+                          <div class="progress">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+                            <span class="sr-only">80% Complete</span>
+                            </div>
+                          </div>
+
+
+												</div>
+												<div class="col-md-6">
+                          <h3>Web Hacking</h3>
+                          Web Hacking.kr
+                          <div class="progress">
+                          	<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                          	<span class="sr-only">60% Complete</span>
+                          	</div>
+                          </div>
+                          Lord of SQL Injection
+                          <div class="progress">
+                            <div class="progress-bar progress-bar-warn" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">
+                            <span class="sr-only">30% Complete</span>
+                            </div>
+                          </div>
+
+
+												</div>
+											</div>
+				                        </div>
+
+				                    </div>
+								</div>
+							</div>
+							<!-- End Profile Tabs -->
+						</div>
+	                </div>
+
+	            </div>
+	        </div>
+		</div>
+
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <nav class="pull-left">
+				<ul>
+					<li>
+						<a href="https://gdb.kr">
+							gdb.kr Co.
+						</a>
+					</li>
+					<li>
+						<a href="https://gdb.kr/me">
+						   About Me
+						</a>
+					</li>
+					<li>
+						<a href="blog.gdb.kr">
+						   Blog
+						</a>
+					</li>
+				</ul>
+            </nav>
+            <div class="copyright pull-right">
+                &copy; 2016, made with <i class="fa fa-heart heart"></i> by Creative Tim
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+	<!--   Core JS Files   -->
+	<script src="../assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="../assets/js/material.min.js"></script>
+
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<script src="../assets/js/nouislider.min.js" type="text/javascript"></script>
+
+	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+	<script src="../assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+	<script src="../assets/js/material-kit.js" type="text/javascript"></script>
+
+</html>
